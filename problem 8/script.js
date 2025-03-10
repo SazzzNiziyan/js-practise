f5b45b795298c3f66442d012473516da1631c02d
Sajal Singh <as3199432@gmail.com>
2025-03-10 11:29:43 +0530

    ques 8 completed

---
+++ problem 8/script.js
@@ -0,0 +1,10 @@
+// .include() method;
+var a ="javaScript"
+
+console.log(a.includes("Script")) // javaScript
+
+// .startsWith() method;
+console.log(a.startsWith("Script")) // true
+
+// .endsWith() method;
+console.log(a.endsWith("Script")) // true

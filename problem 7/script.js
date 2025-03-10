fc8964c6e9acaadd1cc4da0e7c306390241b392c
Sajal Singh <as3199432@gmail.com>
2025-03-10 11:24:03 +0530

    ques7 completed

---
+++ problem 7/script.js
@@ -0,0 +1,16 @@
+var a = "50";
+
+console.log(typeof a)
+
+
+// Convert the string to a number
+
+
+// method 1
+console.log(typeof Number(a))
+
+// method 2
+console.log(typeof parseInt(a))
+
+// method 3
+console.log(typeof +a)

e454241599af07834b04077350602592f26a6b53
Sajal Singh <as3199432@gmail.com>
2025-03-10 11:37:06 +0530

    add new methods in problem 8

--- problem 8/script.js
+++ problem 8/script.js
@@ -8,3 +8,9 @@ console.log(a.startsWith("Script")) // true
 
 // .endsWith() method;
 console.log(a.endsWith("Script")) // true
+
+// indexof() method;
+console.log(a.indexOf("Script") !==-1)// true
+
+// .search() method;
+console.log(a.search("Script") !== -1) // true
